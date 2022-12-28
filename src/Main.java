6f410d973a3c3c250321934b97c9fa7a192592c2
Neto test <loktionov.dv@mail.ru>
2022-12-28 13:33:13 +0300

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,11 @@ public class Main {
 
         int CreditSum = 1_000_000;   //Сумма кредита
 
-        int months1 = 12;       //Количество месяцев
-        int months2 = 24;       //Количество месяцев
-        int months3 = 36;       //Количество месяцев
+        int months = 12;       //Количество месяцев
 
         double rate = 9.99;     //Процентная ставка
 
-        double payment1 = service.calculate1(CreditSum, months1, rate);
-        double payment2 = service.calculate2(CreditSum, months2, rate);
-        double payment3 = service.calculate3(CreditSum, months3, rate);
+        double payment = service.calculate(CreditSum, months, rate);
 
         System.out.println();
         System.out.println("Сумма кредита: " + CreditSum + " Руб");
@@ -23,15 +19,7 @@ public class Main {
         System.out.println();
 
         System.out.printf("Ежемесячный платеж на 12 месяцев составит: ");
-        System.out.printf("%.2f",payment1);
-        System.out.println(" Руб");
-
-        System.out.printf("Ежемесячный платеж на 24 месяца составит : ");
-        System.out.printf("%.2f",payment2);
-        System.out.println(" Руб");
-
-        System.out.printf("Ежемесячный платеж на 36 месяцев составит: ");
-        System.out.printf("%.2f",payment3);
+        System.out.printf("%.2f", payment);
         System.out.println(" Руб");
     }
 }
--- src/MonthlySettlement.java
+++ src/MonthlySettlement.java
@@ -1,35 +1,13 @@
 public class MonthlySettlement {
-    public double calculate1(int CreditSumm, int AmountOfMonths, double interestRate) {
+    public double calculate(int CreditSumm, int AmountOfMonths, double interestRate) {
 
-        double result1;
-        double auxiliaryVariable1 = (interestRate / 12) * 0.01;
-        double auxiliaryVariable2 = 1 + auxiliaryVariable1;
-        double auxiliaryVariable3 = Math.pow(auxiliaryVariable2,AmountOfMonths);
-        double koeff = (auxiliaryVariable1 * auxiliaryVariable3) / (auxiliaryVariable3 - 1);
-        result1 = CreditSumm * koeff;
+        double result;
+        double x = (interestRate / 12) * 0.01;
+        double y = 1 + x;
+        double z = Math.pow(y, AmountOfMonths);
+        double koeff = (x * z) / (z - 1);
+        result = CreditSumm * koeff;
 
-        return result1;
-    }
-    public double calculate2(int CreditSumm, int AmountOfMonths, double interestRate) {
-
-        double result2;
-        double auxiliaryVariable1 = (interestRate / 12) * 0.01;
-        double auxiliaryVariable2 = 1 + auxiliaryVariable1;
-        double auxiliaryVariable3 = Math.pow(auxiliaryVariable2,AmountOfMonths);
-        double koeff = (auxiliaryVariable1 * auxiliaryVariable3) / (auxiliaryVariable3 - 1);
-        result2 = CreditSumm * koeff;
-
-        return result2;
-    }
-    public double calculate3(int CreditSumm, int AmountOfMonths, double interestRate) {
-
-        double result3;
-        double auxiliaryVariable1 = (interestRate / 12) * 0.01;
-        double auxiliaryVariable2 = 1 + auxiliaryVariable1;
-        double auxiliaryVariable3 = Math.pow(auxiliaryVariable2,AmountOfMonths);
-        double koeff = (auxiliaryVariable1 * auxiliaryVariable3) / (auxiliaryVariable3 - 1);
-        result3 = CreditSumm * koeff;
-
-        return result3;
+        return result;
     }
 }
